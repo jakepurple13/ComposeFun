50f67273b13112a8d3b9edd127785004967ec92d
jakepurple13 <jakerein@gmail.com>
2022-04-12 06:52:30 -0400

    - cleanup and navigation integration

--- app/src/main/java/com/programmersbox/composefun/AirBar.kt
+++ app/src/main/java/com/programmersbox/composefun/AirBar.kt
@@ -1,9 +1,13 @@
 package com.programmersbox.composefun
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -15,6 +19,9 @@ import androidx.compose.ui.graphics.*
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import kotlin.random.Random
 
 class AirBarController(
     progress: Double = 0.0,
@@ -90,41 +97,43 @@ fun AirBar(
     val startToEnd = if (controller.animateProgress) animateFloatAsState(horizontal).value else horizontal
 
     Box(modifier = modifier, contentAlignment = if (controller.isHorizontal) Alignment.CenterStart else Alignment.BottomCenter) {
-        Canvas(modifier = Modifier.fillMaxSize().pointerInteropFilter { event ->
-            if (!controller.isHorizontal) {
-                when {
-                    event.y in 0.0..controller.bottomY.toDouble() -> {
-                        valueChanged(calculateValues(event.y, event.x))
-                        true
+        Canvas(modifier = Modifier
+            .fillMaxSize()
+            .pointerInteropFilter { event ->
+                if (!controller.isHorizontal) {
+                    when {
+                        event.y in 0.0..controller.bottomY.toDouble() -> {
+                            valueChanged(calculateValues(event.y, event.x))
+                            true
+                        }
+                        event.y > 100 -> {
+                            valueChanged(calculateValues(event.y, event.x))
+                            true
+                        }
+                        event.y < 0 -> {
+                            valueChanged(calculateValues(event.y, event.x))
+                            true
+                        }
+                        else -> false
                     }
-                    event.y > 100 -> {
-                        valueChanged(calculateValues(event.y, event.x))
-                        true
+                } else {
+                    when {
+                        event.x in 0.0..controller.rightX.toDouble() -> {
+                            valueChanged(calculateValues(event.y, event.x))
+                            true
+                        }
+                        event.x > 100 -> {
+                            valueChanged(calculateValues(event.y, event.x))
+                            true
+                        }
+                        event.x < 0 -> {
+                            valueChanged(calculateValues(event.y, event.x))
+                            true
+                        }
+                        else -> false
                     }
-                    event.y < 0 -> {
-                        valueChanged(calculateValues(event.y, event.x))
-                        true
-                    }
-                    else -> false
                 }
-            } else {
-                when {
-                    event.x in 0.0..controller.rightX.toDouble() -> {
-                        valueChanged(calculateValues(event.y, event.x))
-                        true
-                    }
-                    event.x > 100 -> {
-                        valueChanged(calculateValues(event.y, event.x))
-                        true
-                    }
-                    event.x < 0 -> {
-                        valueChanged(calculateValues(event.y, event.x))
-                        true
-                    }
-                    else -> false
-                }
-            }
-        }) {
+            }) {
             controller.bottomY = size.height
             controller.rightX = size.width
             if (controller.internalProgress > 0.0) controller.progress = controller.internalProgress
@@ -285,11 +294,167 @@ fun AirBar(
 @ExperimentalComposeUiApi
 @Composable
 @Preview
-fun Preview() {
-    Column {
-        AirBar { }
+fun AirBarLayout(navController: NavController = rememberNavController()) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(Screen.AirBarScreen.name) },
+                navigationIcon = { IconButton(onClick = { navController.popBackStack() }) { Icon(Icons.Default.ArrowBack, null) } }
+            )
+        }
+    ) { p ->
+        val airBar = rememberAirBarController(45.0)
+        val airBar2 = rememberAirBarController(90.0, isHorizontal = true)
+        val airBar3 = rememberAirBarController(40.0, isHorizontal = true)
+        val airBar4 = rememberAirBarController(40.0)
+        var progress by remember { mutableStateOf(35f) }
 
-        var progress by remember { mutableStateOf(0f) }
-        AirBar(progress = progress) { progress = it }
+        var randomNumber by remember { mutableStateOf(50.0) }
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier.padding(p)
+        ) {
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("${String.format("%.2f", animateFloatAsState(targetValue = airBar.progress.toFloat()).value)}%")
+                Text("${String.format("%.2f", animateFloatAsState(targetValue = airBar2.progress.toFloat()).value)}%")
+                Text("${String.format("%.2f", progress)}%")
+            }
+            val prim = MaterialTheme.colors.primary
+            val back = Color.Blue
+
+            var primaryColor by remember { mutableStateOf(prim) }
+            var backgroundColor by remember { mutableStateOf(back) }
+
+            val primary by animateColorAsState(primaryColor)
+            val background by animateColorAsState(backgroundColor)
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+
+                Button(
+                    onClick = {
+                        airBar.progress = randomNumber
+                        airBar2.progress = randomNumber
+                        airBar3.progress = randomNumber
+                        airBar4.progress = randomNumber
+                        progress = randomNumber.toFloat()
+                        randomNumber = Random.nextDouble(0.0, 100.0)
+                    }
+                ) { Text("Set to ${String.format("%.2f", randomNumber).toDouble()}%") }
+
+                Text("Animate")
+                Switch(
+                    checked = airBar.animateProgress && airBar2.animateProgress,
+                    onCheckedChange = {
+                        airBar.animateProgress = it
+                        airBar2.animateProgress = it
+                    }
+                )
+
+                Button(
+                    onClick = {
+                        primaryColor = Color(Random.nextInt(0, 255), Random.nextInt(0, 255), Random.nextInt(0, 255), 255)
+                        backgroundColor = Color(Random.nextInt(0, 255), Random.nextInt(0, 255), Random.nextInt(0, 255), 255)
+                    }
+                ) { Text("Random Color") }
+
+            }
+
+            AirBar(
+                controller = airBar,
+                modifier = Modifier
+                    .height(200.dp)
+                    .width(80.dp),
+                fillColor = primary,
+                backgroundColor = background,
+                icon = { Icon(Icons.Default.Refresh, null) }
+            ) { airBar.progress = it }
+
+            Row {
+                Column {
+                    AirBar(
+                        controller = airBar2,
+                        modifier = Modifier
+                            .width(200.dp)
+                            .height(80.dp),
+                        backgroundColor = background,
+                        fillColor = primary,
+                        icon = { Icon(Icons.Default.Refresh, null) }
+                    ) { airBar2.progress = it }
+
+                    AirBar(
+                        controller = airBar2,
+                        modifier = Modifier
+                            .width(200.dp)
+                            .height(80.dp),
+                        backgroundColor = primary,
+                        fillColor = background,
+                        icon = { Icon(Icons.Default.Refresh, null) }
+                    ) { airBar2.progress = it }
+                }
+
+                AirBar(
+                    controller = airBar3,
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(horizontal = 4.dp)
+                        .height(160.dp),
+                    backgroundColor = background,
+                    fillColor = primary
+                )
+
+            }
+
+            Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+                AirBar(
+                    if (airBar2.animateProgress && airBar.animateProgress)
+                        animateFloatAsState(targetValue = airBar.progress.toFloat()).value
+                    else
+                        airBar.progress.toFloat(),
+                    modifier = Modifier
+                        .height(200.dp)
+                        .width(80.dp),
+                    backgroundColor = primary,
+                    fillColor = background,
+                    icon = { Icon(Icons.Default.Refresh, null) }
+                ) { airBar.progress = it.toDouble() }
+
+                AirBar(
+                    if (airBar2.animateProgress && airBar.animateProgress)
+                        animateFloatAsState(targetValue = progress).value
+                    else
+                        progress,
+                    modifier = Modifier
+                        .height(200.dp)
+                        .width(80.dp),
+                    backgroundColor = primary,
+                    fillColor = background,
+                    icon = { Text("${String.format("%.2f", progress)}%") }
+                ) { progress = it }
+
+                AirBar(
+                    controller = airBar4,
+                    backgroundColor = background,
+                    fillColor = primary,
+                    modifier = Modifier
+                        .height(200.dp)
+                        .width(80.dp)
+                )
+
+                AirBar(
+                    controller = rememberAirBarController(40.0),
+                    backgroundColor = background,
+                    fillColor = primary
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/programmersbox/composefun/MainActivity.kt
+++ app/src/main/java/com/programmersbox/composefun/MainActivity.kt
@@ -3,23 +3,19 @@ package com.programmersbox.composefun
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.programmersbox.composefun.ui.theme.ComposeFunTheme
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalComposeUiApi::class)
@@ -28,159 +24,34 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeFunTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    // A surface container using the 'background' color from the theme
-                    val airBar = rememberAirBarController(45.0)
-                    val airBar2 = rememberAirBarController(90.0, isHorizontal = true)
-                    val airBar3 = rememberAirBarController(40.0, isHorizontal = true)
-                    val airBar4 = rememberAirBarController(40.0)
-                    var progress by remember { mutableStateOf(35f) }
 
-                    var randomNumber by remember { mutableStateOf(50.0) }
+                    val navController = rememberNavController()
 
-                    Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-
-                        Row(
-                            horizontalArrangement = Arrangement.SpaceEvenly,
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            Text("${String.format("%.2f", animateFloatAsState(targetValue = airBar.progress.toFloat()).value)}%")
-                            Text("${String.format("%.2f", animateFloatAsState(targetValue = airBar2.progress.toFloat()).value)}%")
-                            Text("${String.format("%.2f", progress)}%")
-                        }
-                        val p = MaterialTheme.colors.primary
-                        val back = Color.Blue
-
-                        var primaryColor by remember { mutableStateOf(p) }
-                        var backgroundColor by remember { mutableStateOf(back) }
-
-                        val primary by animateColorAsState(primaryColor)
-                        val background by animateColorAsState(backgroundColor)
-
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.SpaceEvenly,
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-
-                            Button(
-                                onClick = {
-                                    airBar.progress = randomNumber
-                                    airBar2.progress = randomNumber
-                                    airBar3.progress = randomNumber
-                                    airBar4.progress = randomNumber
-                                    progress = randomNumber.toFloat()
-                                    randomNumber = Random.nextDouble(0.0, 100.0)
-                                }
-                            ) { Text("Set to ${String.format("%.2f", randomNumber).toDouble()}%") }
-
-                            Text("Animate")
-                            Switch(
-                                checked = airBar.animateProgress && airBar2.animateProgress,
-                                onCheckedChange = {
-                                    airBar.animateProgress = it
-                                    airBar2.animateProgress = it
-                                }
-                            )
-
-                            Button(
-                                onClick = {
-                                    primaryColor = Color(Random.nextInt(0, 255), Random.nextInt(0, 255), Random.nextInt(0, 255), 255)
-                                    backgroundColor = Color(Random.nextInt(0, 255), Random.nextInt(0, 255), Random.nextInt(0, 255), 255)
-                                }
-                            ) { Text("Random Color") }
-
-                        }
-
-                        AirBar(
-                            controller = airBar,
-                            modifier = Modifier
-                                .height(200.dp)
-                                .width(80.dp),
-                            fillColor = primary,
-                            backgroundColor = background,
-                            icon = { Icon(Icons.Default.Refresh, null) }
-                        ) { airBar.progress = it }
-
-                        Row {
-                            Column {
-                                AirBar(
-                                    controller = airBar2,
-                                    modifier = Modifier
-                                        .width(200.dp)
-                                        .height(80.dp),
-                                    backgroundColor = background,
-                                    fillColor = primary,
-                                    icon = { Icon(Icons.Default.Refresh, null) }
-                                ) { airBar2.progress = it }
-
-                                AirBar(
-                                    controller = airBar2,
-                                    modifier = Modifier
-                                        .width(200.dp)
-                                        .height(80.dp),
-                                    backgroundColor = primary,
-                                    fillColor = background,
-                                    icon = { Icon(Icons.Default.Refresh, null) }
-                                ) { airBar2.progress = it }
-                            }
-
-                            AirBar(
-                                controller = airBar3,
-                                modifier = Modifier
-                                    .align(Alignment.CenterVertically)
-                                    .padding(horizontal = 4.dp)
-                                    .height(160.dp),
-                                backgroundColor = background,
-                                fillColor = primary
-                            )
-
-                        }
-
-                        Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
-                            AirBar(
-                                if (airBar2.animateProgress && airBar.animateProgress)
-                                    animateFloatAsState(targetValue = airBar.progress.toFloat()).value
-                                else
-                                    airBar.progress.toFloat(),
-                                modifier = Modifier
-                                    .height(200.dp)
-                                    .width(80.dp),
-                                backgroundColor = primary,
-                                fillColor = background,
-                                icon = { Icon(Icons.Default.Refresh, null) }
-                            ) { airBar.progress = it.toDouble() }
-
-                            AirBar(
-                                if (airBar2.animateProgress && airBar.animateProgress)
-                                    animateFloatAsState(targetValue = progress).value
-                                else
-                                    progress,
-                                modifier = Modifier
-                                    .height(200.dp)
-                                    .width(80.dp),
-                                backgroundColor = primary,
-                                fillColor = background,
-                                icon = { Text("${String.format("%.2f", progress)}%") }
-                            ) { progress = it }
-
-                            AirBar(
-                                controller = airBar4,
-                                backgroundColor = background,
-                                fillColor = primary,
-                                modifier = Modifier
-                                    .height(200.dp)
-                                    .width(80.dp)
-                            )
-
-                            AirBar(
-                                controller = rememberAirBarController(40.0),
-                                backgroundColor = background,
-                                fillColor = primary
-                            )
-                        }
+                    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+                        composable(Screen.MainScreen.route) { MainScreen(navController = navController) }
+                        composable(Screen.AirBarScreen.route) { AirBarLayout(navController) }
                     }
+
                 }
             }
         }
     }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun MainScreen(navController: NavController) {
+    Scaffold(topBar = { TopAppBar(title = { Text(Screen.MainScreen.name)}) }) {
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(3),
+            contentPadding = it
+        ) {
+            item { Button(onClick = { navController.navigate(Screen.AirBarScreen.route) }) { Text(Screen.AirBarScreen.name) } }
+        }
+    }
+}
+
+sealed class Screen(val route: String, val name: String) {
+    object AirBarScreen : Screen("airbar", "AirBar")
+    object MainScreen : Screen("mainscreen", "Playground")
 }
